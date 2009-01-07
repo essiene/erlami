af4683c5dfde1c381c1d2b5453b706f3ec46757d
Essien Ita Essien <essiene@gmail.com>
2009-01-07 13:40:45 +0100

    Add gen_fsm stumps to ami_socket and hook up to build system

--- src/lib/ami_socket.erl
+++ src/lib/ami_socket.erl
@@ -3,9 +3,15 @@
 % when the endpoint comes back online.
 %
 % States are:
-%   Open-NotConnected
-%   Open-Connected
-%   Closed
+%   connected : Open and connected to end point
+%               Recv works, Send works, async notifications
+%               recv'd on active|once|true recieves.
+
+%   disconnected: Open but not connected to end point
+%               Recv gives an error (not closed), Send gives
+%               an error, No asnc notifications are recv'd
+%
+%   closed    : Closed and not able to communicate.
 % 
 % This socket mimics the semantics of a normal gen_tcp socket
 % connection and can operate in active -> true | once| false modes
@@ -17,3 +23,43 @@
 %
 -module(ami_socket).
 -behaviour(gen_fsm).
+
+-export([
+%        connect/3
+%        connect/4,
+%        send/2,
+%        recv/2,
+%        recv/3,
+%        controlling_process/2,
+%        close/1,
+%        shutdown/2
+    ]).
+
+-export([
+        init/1,
+        handle_sync_event/4,
+        handle_event/3,
+        handle_info/3,
+        terminate/3,
+        code_change/4
+    ]).
+
+
+init([]) ->
+    {ok, disconnected, nil}.
+
+
+handle_sync_event(Event, _From, StateName, St) ->
+    {reply, {illegal_event, Event}, StateName, St}.
+
+handle_event(_Event, StateName, St) ->
+    {next_state, StateName, St}.
+
+handle_info(_Info, StateName, St) ->
+    {next_state, StateName, St}.
+
+terminate(_Reason, _StateName, _St) ->
+    ok.
+
+code_change(_OldVsn, StateName, St, _Extra) ->
+    {next_state, StateName, St}.
