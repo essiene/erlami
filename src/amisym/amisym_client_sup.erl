0da541a3822c5ffab46fd620b777393b46cb1fec
trinisoftinc <trinisoftinc@gmail.com>
2009-01-12 13:04:15 +0100

    Added the client supervisor.

---
+++ src/amisym/amisym_client_sup.erl
@@ -0,0 +1,39 @@
+-module(amisym_client_sup).
+-behaviour(supervisor).
+
+-include("ami.hrl")
+
+-export([
+            start_link/0,
+            start_child/1
+        ]).
+
+-export([
+            init/1
+        ]).
+
+
+start_link() ->
+    Res = supervisor:start_link({?local, ?CLIENT_SUP}, ?MODULE, []),
+    error:logger:info_report({?MODULE, started}),
+    Res.
+
+start_child(ClientSocket) ->
+    supervisor:start_child(?CLIENT_SUP, [ClientSocket]).
+
+init([]) ->
+    {ok,
+        {
+            {simple_one_for_one, 3, 10},
+            [
+                {
+                    amisym_client_sup,
+                    {amisym_client_fsm, start_link, []},
+                    transient,
+                    5000,
+                    worker,
+                    [amisym_client_fsm]
+                }
+            ]
+        }
+    }.
